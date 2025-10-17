55666743f55f5de300deca1e6de48c23f972f3dd
Liam Clark <liam.clark@lstudios-media.de>
2025-10-17 11:00:53 +0200

    Update camera_overlay.py

--- madpi_ws/src/mbmadpi/overlay/camera_overlay.py
+++ madpi_ws/src/mbmadpi/overlay/camera_overlay.py
@@ -81,7 +81,7 @@ class CarState:
                     'pos': rec.get('pos', None),
                     'driver': rec.get('driver', '') or f'car_{cid}',
                     'lap': rec.get('lapctr'),
-                    'time': rec.get('currentlaptime', 0.0),
+                    'time': rec.get('laptime', 0.0),
                     'avgspeed': rec.get('avgspeed', 0.0),
                     'mode': 'On Track',
                 }
